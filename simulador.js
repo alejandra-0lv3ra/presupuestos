043524fdc8cb3574d7cd8f7cb71a6a5cd6dd7a25
Ale Olvera <alejandraolv.28@gmail.com>
2023-11-14 23:15:39 -0600

    start of project

---
+++ simulador.js
@@ -0,0 +1,26 @@
+function calcularPresupuesto() {
+    let ingresos = 0;
+    let gastos = 0;
+
+    // Ingresos
+    while (true) {
+        ingresos = +prompt("Ingresa tu presupuesto");
+        while  (ingreso !== 1,2,3,4,5,6,7,8,9) {
+            alert("Ingresa un valor numérico válido.");
+        }
+       // Gastos
+    while (true) {
+        gastos = +prompt("Ingresa tu presupuesto");
+        while  (gastos !== 1,2,3,4,5,6,7,8,9) {
+            alert("Ingresa un valor numérico válido.");
+        }
+    }
+    }  
+}
+
+// Calcular el presupuesto
+const presupuesto = ingresos - gastos;
+
+// Mostrar el resultado
+let resultado = 
+alert("el resultado es $${presupuesto}"); 
